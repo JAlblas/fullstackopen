b6dfb9c1a81461a8530dc27015869dca3679d51a
Jasper Alblas <just_jasper@hotmail.com>
2023-05-18 21:53:58 +0200

    unicafe step 1 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,13 +4,13 @@ const Button = ({ text, handleClick }) => {
   return (<button onClick={handleClick}> {text} </button >)
 }
 
-const Statistics = () => {
+const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>Statistics</h2>
-      <p>Good:</p>
-      <p>Neutral:</p>
-      <p>Bad:</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad} </p>
     </div >
   )
 }
@@ -24,11 +24,11 @@ const App = () => {
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button text={"1"} handleClick={() => console.log('HIHI')}></Button>
-      <Button text={"2"} handleClick={() => console.log('HIHI')}></Button>
-      <Button text={"3"} handleClick={() => console.log('HIHI')}></Button>
-      <Statistics></Statistics>
-    </div>
+      <Button text={"Good"} handleClick={() => setGood(good + 1)}></Button>
+      <Button text={"Neutral"} handleClick={() => setNeutral(neutral + 1)}></Button>
+      <Button text={"Bad"} handleClick={() => setBad(bad + 1)}></Button>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+    </div >
   )
 }
 
