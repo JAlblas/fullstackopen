030b08948c5139e43adcbdf62e2483990fcf39f0
Jasper Alblas <just_jasper@hotmail.com>
2023-05-20 11:35:18 +0200

    Course component seperated in its own module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,40 +1,4 @@
-const Header = (props) => {
-    return <h1>{props.course}</h1>
-}
-
-const Part = ({ name, exercises }) => {
-    return (
-        <div>
-            <p>
-                {name}: {exercises}
-            </p>
-        </div>
-    )
-}
-
-const Content = ({ parts }) => {
-    return (
-        <div>
-            {parts.map(
-                part => { return (<Part name={part.name} exercises={part.exercises} key={part.id} />) }
-            )}
-        </div>
-    )
-}
-
-const Total = (props) => {
-    return <p> <b>Total number of exercises:</b> {props.total} </p>
-}
-
-const Course = ({ course, exercisesNumber }) => {
-    return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total total={exercisesNumber} />
-        </div>
-    )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
@@ -97,7 +61,5 @@ const App = () => {
         </div>
     )
 }
-
-
 
 export default App
