05ee599100042d6bd8d6123f13a5728e48ee3558
Jasper Alblas <just_jasper@hotmail.com>
2023-11-03 15:19:51 +0100

    unicafe step 1 done

--- part1/unicafe_new/src/App.jsx
+++ part1/unicafe_new/src/App.jsx
@@ -6,9 +6,20 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h2>Statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
     </div>
   )
 }
