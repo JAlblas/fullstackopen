8493d75cb733cf022107cc78ca5d5839206a82a3
Jasper Alblas <just_jasper@hotmail.com>
2023-11-04 10:53:39 +0100

    unicafe step 3 done

--- part1/unicafe_new/src/App.jsx
+++ part1/unicafe_new/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const percentageGood = (good / total) * 100;
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>total: {total}</p>
+      <p>average: {average.toFixed(2)}</p>
+      <p>positive: {percentageGood.toFixed(2)}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const percentageGood = good / total;
 
   const handleGood = () => setGood(good + 1)
   const handleNeutral = () => setNeutral(neutral + 1)
@@ -20,13 +32,10 @@ const App = () => {
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      <h2>Statistics</h2>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
-      <p>total: {total}</p>
-      <p>average: {average.toFixed(2)}</p>
-      <p>positive: {percentageGood.toFixed(2)}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
