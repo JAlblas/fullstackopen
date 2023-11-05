82a40a5cbccb962bf8e1ce0c302d555d74c9d659
Jasper Alblas <just_jasper@hotmail.com>
2023-11-05 12:11:13 +0100

    phonebook step 3 done. Numbers can now be saved

--- part2/phonebook_new/src/App.jsx
+++ part2/phonebook_new/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -17,12 +22,14 @@ const App = () => {
       return
     } else {
       const personObject = {
-        name: newName,
         id: persons.length + 1,
+        name: newName,
+        number: newNumber
       }
 
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -33,6 +40,7 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
