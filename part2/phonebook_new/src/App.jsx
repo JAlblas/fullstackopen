962ac5cb11267e745591e94d68124aadbe9cc83f
Jasper Alblas <just_jasper@hotmail.com>
2023-11-07 21:38:10 +0100

    Phonebook step 8 done. Backend communication extracted to its own module

--- part2/phonebook_new/src/App.jsx
+++ part2/phonebook_new/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import personService from './services/persons'
+
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -13,9 +15,9 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService.getAll()
+      .then(persons => {
+        setPersons(persons)
       })
   }, [])
 
@@ -31,9 +33,9 @@ const App = () => {
         number: newNumber
       }
 
-      axios.post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      personService.create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
         })
