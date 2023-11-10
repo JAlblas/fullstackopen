852fea1aa55416cf529c8487fcd6ba50c512133d
Jasper Alblas <just_jasper@hotmail.com>
2023-11-10 12:59:01 +0100

    If person already is removed, it now gets removed from state upon removal

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -107,7 +107,7 @@ const App = () => {
           setTimeout(() => {
             setErrorMessage(null)
           }, 5000)
-          setNotes(notes.filter(n => n.id !== id))
+          setPersons(persons.filter(n => n.id !== id))
         })
     }
   }
