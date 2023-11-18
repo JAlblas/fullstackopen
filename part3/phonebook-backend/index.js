b07498079af74538edc71ea91e35ac45e1fe13c9
Jasper Alblas <just_jasper@hotmail.com>
2023-11-18 22:24:24 +0100

    phonebook api step 3 done. Fetching of individual persons possible

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,7 +25,7 @@ let persons = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!!!</h1>')
+    response.send('<h1>Phonebook API</h1>')
 })
 
 app.get('/info', (request, response) => {
@@ -34,6 +34,17 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
